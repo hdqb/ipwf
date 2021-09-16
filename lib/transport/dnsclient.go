a2f03be3f3e157a0a1a4a05661a55b2fba8605a7
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 05:02:41 +0700

    up

--- lib/transport/dnsclient.go
+++ lib/transport/dnsclient.go
@@ -8,9 +8,9 @@ import (
 func sendDNSQuery(data []byte, target string) (responses []string, err error) {
 	// We use TXT requests to tunnel data. Feel free to implement your own method.
 	send := fmt.Sprintf("%s.%s", data, target)
-	fmt.Println("send : ", send)
+	// fmt.Println("send : ", send)
 	responses, err = net.LookupTXT(send)
-	fmt.Println("res : ", responses)
+	// fmt.Println("res : ", responses)
 
 	return
 }
