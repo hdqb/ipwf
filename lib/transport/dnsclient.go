3c4600d50e990dc20c9a6b49d36091c7a6f2e166
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 08:44:10 +0700

    up

--- lib/transport/dnsclient.go
+++ lib/transport/dnsclient.go
@@ -1,43 +1,38 @@
 package transport
 
 import (
+	"context"
 	"fmt"
 	"net"
-	"time"
-
-	"github.com/miekg/dns"
 )
 
 func sendDNSQuery(data []byte, target string) (responses []string, err error) {
 	// We use TXT requests to tunnel data. Feel free to implement your own method.
 	send := fmt.Sprintf("%s.%s", data, target)
 	// fmt.Println("send : ", send)
-	responses, err = net.LookupTXT(send)
+	responses, err = LookupTXT(send)
 	// fmt.Println("res : ", responses)
 
 	return
 }
 
-func LookupTXT(host string) ([]string, error) {
+func LookupTXT(send string) ([]string, error) {
 
-	m1 := new(dns.Msg)
-	m1.Id = dns.Id()
-	m1.RecursionDesired = true
-	m1.Question = make([]dns.Question, 1)
-	m1.Question[0] = dns.Question{host, dns.TypeTXT, dns.ClassINET}
-	c := new(dns.Client)
-	laddr := net.UDPAddr{
-		IP:   net.ParseIP("[::1]"),
-		Port: 0,
-		Zone: "",
+	r := net.Resolver{
+		PreferGo: true,
+
+		Dial: GoogleDNSDialer,
 	}
-
-	c.Dialer = &net.Dialer{
-		Timeout:   200 * time.Millisecond,
-		LocalAddr: &laddr,
+	ctx := context.Background()
+	ipaddr, err := r.LookupTXT(ctx, send)
+	if err != nil {
+		panic(err)
 	}
-	in, rtt, err := c.Exchange(m1, "8.8.8.8:53")
+	fmt.Println("DNS Result", ipaddr)
+	return ipaddr, nil
+}
 
-	fmt.Println(in, rtt, err)
-
+func GoogleDNSDialer(ctx context.Context, network, address string) (net.Conn, error) {
+	d := net.Dialer{}
+	return d.DialContext(ctx, "udp", "8.8.8.8:53")
 }
