483284c3a473adbef3ac3db5c69d2d21eea20246
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 08:41:29 +0700

    up

--- lib/transport/dnsclient.go
+++ lib/transport/dnsclient.go
@@ -3,6 +3,9 @@ package transport
 import (
 	"fmt"
 	"net"
+	"time"
+
+	"github.com/miekg/dns"
 )
 
 func sendDNSQuery(data []byte, target string) (responses []string, err error) {
@@ -13,4 +16,28 @@ func sendDNSQuery(data []byte, target string) (responses []string, err error) {
 	// fmt.Println("res : ", responses)
 
 	return
+}
+
+func LookupTXT(host string) ([]string, error) {
+
+	m1 := new(dns.Msg)
+	m1.Id = dns.Id()
+	m1.RecursionDesired = true
+	m1.Question = make([]dns.Question, 1)
+	m1.Question[0] = dns.Question{host, dns.TypeTXT, dns.ClassINET}
+	c := new(dns.Client)
+	laddr := net.UDPAddr{
+		IP:   net.ParseIP("[::1]"),
+		Port: 0,
+		Zone: "",
+	}
+
+	c.Dialer = &net.Dialer{
+		Timeout:   200 * time.Millisecond,
+		LocalAddr: &laddr,
+	}
+	in, rtt, err := c.Exchange(m1, "8.8.8.8:53")
+
+	fmt.Println(in, rtt, err)
+
 }
